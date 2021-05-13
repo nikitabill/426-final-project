d7096fc66c79e1f26aede347a3961332cb26ce02
nikitabill <nikitale@live.unc.edu>
2021-05-13 14:46:41 -0400

    Login works with Google but logout has uncaught error

--- script.js
+++ script.js
@@ -53,16 +53,8 @@ document.addEventListener("DOMContentLoaded", () => { // our default page will b
 
     // Adding Google login 
     document.getElementById("google").addEventListener("click", (e) => {
-        onSignIn(); 
-    }
-
-    function onSignIn(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    }; 
+        
+    }); 
 
 
     // Handles createAccount button press; pulls up AccRegistration form; makes Login form disappear
